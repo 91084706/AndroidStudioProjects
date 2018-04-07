9664a8c328735fd8346843dd5e33bf7259a5cf76
palmdigital <palmdigital@PLMS-SC1-S19.mvusd.k12.ca.us>
2018-04-07 08:58:08 -0700

    continued Choose Your Own Adventure

--- ChooseYourOwnAdventure_v01/app/src/main/java/com/example/palmdigital/chooseyourownadventure_v01/ToiletActivity.java
+++ ChooseYourOwnAdventure_v01/app/src/main/java/com/example/palmdigital/chooseyourownadventure_v01/ToiletActivity.java
@@ -2,12 +2,22 @@ package com.example.palmdigital.chooseyourownadventure_v01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ToiletActivity extends AppCompatActivity {
+public class ToiletActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toilet);
+        Button buttonYesObject = findViewById(R.id.buttonLeft_Yes);
+        Button buttonHeckYesObject = findViewById(R.id.buttonRight_HeckYes);
+        buttonHeckYesObject.setOnClickListener(this);
+        buttonYesObject.setOnClickListener(this);
+    }
+    public void OnClick (View v)
+    {
+        if ()
     }
 }
