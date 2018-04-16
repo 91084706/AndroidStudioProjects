9d52a4b401d6cab90ee807f0ff7631c225b49050
palmdigital <palmdigital@PLMS-SC1-S19.mvusd.k12.ca.us>
2018-04-16 10:54:43 -0700

    made a new MakeStory activity

--- MadLibs_Summer_v01/app/src/main/java/com/example/palmdigital/madlibs_summer_v01/MainActivity.java
+++ MadLibs_Summer_v01/app/src/main/java/com/example/palmdigital/madlibs_summer_v01/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.palmdigital.madlibs_summer_v01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button makeStory = findViewById(R.id.bt_makeStory);
+    }
+    public void onClick(View v)
+    {
+        if(v.getId() == R.id.bt_makeStory)
+        {
+            Intent i = new Intent(this, MakeStory.class);
+            startActivity(i);
+        }
     }
 }
