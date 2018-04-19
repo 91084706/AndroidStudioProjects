9468cfce3066594993ad02d4277bf14f2c363726
palmdigital <palmdigital@PLMS-SC1-S19.mvusd.k12.ca.us>
2018-04-19 14:17:08 -0700

    summer

--- MadLibs_Summer_v01/app/src/main/java/com/example/palmdigital/madlibs_summer_v01/MainActivity.java
+++ MadLibs_Summer_v01/app/src/main/java/com/example/palmdigital/madlibs_summer_v01/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -13,9 +15,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button makeStory = findViewById(R.id.button);
+        makeStory.setOnClickListener(this);
+
     }
     public void onClick(View v)
     {
+        EditText etName = findViewById(R.id.et_name);
+        String nameStr = etName.getText().toString();
 
+        EditText etPlace1 = findViewById(R.id.et_place1);
+        String placeStr = etPlace1.getText().toString();
+
+        String story = "Last summer my mom and dad took me and ";
+        story += nameStr;
+        story += "on a trip to ";
+        story += placeStr;
+        story += ". The weather there is very ";
+
+        TextView output = findViewById(R.id.tvStory);
+        output.setText(story);
     }
 }
